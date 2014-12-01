104af7b13ee89bc7703fadba23577ca3aa18b31d
Yannick Berendsen <mryannickz@live.nl>
2014-12-01 11:34:41 +0100

    Start gemaakt met dashboard, view van dashboard aangemaakt, css & html naar laravel gezet

---
+++ app/views/dashboard/main.blade.php
@@ -0,0 +1,28 @@
+<section class="ac-container">
+  <div>
+    <input id="ac-1" name="accordion-1" type="radio" checked />
+    <label for="ac-1">Voortgang van het project (Logtime)</label>
+    <article class="ac-small"> dsf </article>
+  </div>
+  <div>
+    <input id="ac-2" name="accordion-1" type="radio" />
+    <label for="ac-2">Voortgang van het project (Pizza today)</label>
+    <article class="ac-small"> sdf </article>
+  </div>
+</section>
+</div>
+{{HTML::script(asset('js/notificatie.js')) }}
+{{HTML::script(asset('js/menuleft.js')) }}
+</body></html><!---------------------Credits---------------------
+
+Class:          4D0W
+Webdesign:      Fatih celik
+Mobile design:  Dennis eilander
+Programmers:    Thiago van Dieten
+                Phillip Heemskerk
+                Yannick Berendsen
+                Fatih Celik
+                Dennis Eilander
+
+© 2014-2015 by Orange source. All Rights Reserved.
+-------------------------------------------------->
