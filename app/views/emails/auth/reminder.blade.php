1b6924192f4f6cd11b3bf403f28a706fce96d4d5
Fatih Celik <celik75@live.nl>
2014-12-09 14:26:30 +0100

    Fix voor lettertype en kleur ect.

--- app/views/emails/auth/reminder.blade.php
+++ app/views/emails/auth/reminder.blade.php
@@ -6,7 +6,7 @@
 	<body>
 		<h2>Wachtwoord aanpassen</h2>
 
-		<div>
+		<div style="color: #666; font-size: 16px;font-family: arial; line-height: 24px;">
 			Dit bericht is een reactie op uw verzoek het wachtwoord voor uw account opnieuw in te stellen. Klik op de onderstaande link en volg de aanwijzingen.<br/>{{ URL::to('password/reset', array($token)) }}
 			Deze link verloopt over {{ Config::get('auth.reminder.expire', 60) }} minuten.
 		</div>
