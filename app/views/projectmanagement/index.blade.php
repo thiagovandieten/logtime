2b59e5898ef1a35c573f52c21635bb1c7c421fe6
Fatih Celik <celik75@live.nl>
2014-12-17 14:36:15 +0100

    Filter balk met buttons toegepast en responsive gemaakt

--- app/views/projectmanagement/index.blade.php
+++ app/views/projectmanagement/index.blade.php
@@ -1,2 +1,25 @@
 @extends('template.main')
 
+@section('content')
+<div class="filter-wrap">
+    <div class="buttons-wrap">
+        <button class="nieuw-knop">Nieuw</button>
+        <button class="delete-knop" style="margin-left: 5px;">Verwijderen</button>
+    </div>
+    <div class="filter-omgeving">
+    <p>Filter op</p>
+    <form>
+    <select>
+     <option>Leerjaar</option>
+    </select>
+    <select>
+      <option>Periode</option>
+     </select>
+     <select>
+       <option>Klas</option>
+     </select>
+    </form>
+    </div>
+</div>
+
+@stop
