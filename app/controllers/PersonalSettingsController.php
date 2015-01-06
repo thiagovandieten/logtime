82c69feab7e46aebfa09efe20c2c1eede31c395b
Dennis Eilander <eilander.dennis@gmail.com>
2015-01-06 15:08:08 +0100

    Adresgegevens ophalen en wegschrijven naar database

--- app/controllers/PersonalSettingsController.php
+++ app/controllers/PersonalSettingsController.php
@@ -11,9 +11,25 @@ class PersonalSettingsController extends BaseLoggedInController {
         
     public function index()
     {        
-        $user = User::find(Auth::id());    
+        $user           = User::find(Auth::id());   
+        $adress_id      = Adress::find($user->adress_id);
+        
+        $street_id      = $adress_id->street_id;
+        $street         = Street::find($street_id);
+        
+        $city_id        = $adress_id->city_id;
+        $city           = City::find($city_id);
+        
+        $zipcode_id     = $adress_id->zipcode_id;
+        $zipcode        = Zipcode::find($zipcode_id);
+        
+        //dd($zipcode->zipcode);
+    
+        
         $personal_data = array('first_name' => $user->first_name, 'last_name' => $user->last_name, 
-                               'phone_number' => $user->phone_number, 'avatar' => $user->user_image_path);
+                               'phone_number' => $user->phone_number, 'avatar' => $user->user_image_path, 
+                               'street' => $street->street, 'house_number' => $street->house_number,
+                               'city' => $city->city, 'zipcode' => $zipcode->zipcode);
 
         return View::make('personal_settings')->with(array
             ('personal_data' => $personal_data));
@@ -22,15 +38,25 @@ class PersonalSettingsController extends BaseLoggedInController {
     public function store()
     {
         $user = User::find(Auth::id());
+        $adress_id      = Adress::find($user->adress_id);
+        
+        $street_id      = $adress_id->street_id;
+        $street         = Street::find($street_id);
+        
+        $city_id        = $adress_id->city_id;
+        $city           = City::find($city_id);
+        
+        $zipcode_id     = $adress_id->zipcode_id;
+        $zipcode        = Zipcode::find($zipcode_id);
         
         //Data for image upload -------------------------------
         if(Input::hasFile('avatar')){
-            $destinatonPath = '';
-            $filename = '';
-            $file = Input::file('avatar');
-            $destinationPath = public_path().'/images/';
-            $filename = str_random(6).'_'.$file->getClientOriginalName();
-            $uploadSuccess = $file->move($destinationPath, $filename);
+            $destinatonPath     = '';
+            $filename           = '';
+            $file               = Input::file('avatar');
+            $destinationPath    = public_path().'/images/';
+            $filename           = str_random(6).'_'.$file->getClientOriginalName();
+            $uploadSuccess      = $file->move($destinationPath, $filename);
         }else{
             $filename = $user->user_image_path;    
         }
@@ -39,6 +65,10 @@ class PersonalSettingsController extends BaseLoggedInController {
         $rules = array(
             'first_name'       => 'required', 						// just a normal required validation
             'last_name'        => 'required', 	                    // just a normal required validation
+            'street'           => 'required',                       // just a normal required validation
+            'house_number'     => 'required', 						// just a normal required validation
+            'zipcode'          => 'required', 	                    // just a normal required validation
+            'city'             => 'required', 	                    // just a normal required validation
             'phone_number'     => 'required'                        // just a normal required validation
             //'password'         => 'required',
             //'password_confirm' => 'required|same:password' 		// required and has to match the password field
@@ -50,13 +80,14 @@ class PersonalSettingsController extends BaseLoggedInController {
 
         // check if the validator failed -----------------------
         if ($validator->fails()) {
-
             // get the error messages from the validator
             $messages = $validator->messages('Er is iets fout gegaan');
 
             // redirect our user back to the form with the errors from the validator
             return Redirect::to('persoonlijke-instellingen')
                 ->withErrors($validator);
+            
+            //dd($validator);
 
         } else {
                         
@@ -69,11 +100,21 @@ class PersonalSettingsController extends BaseLoggedInController {
             $user_data = User::find(Auth::id());
             $user_data->first_name       = Input::get('first_name');
             $user_data->last_name        = Input::get('last_name');
-            $user_data->phone_number     = Input::get('phone_number');
+            
+            $street->street             = Input::get('street');
+            $street->house_number       = Input::get('house_number');
+            $zipcode->zipcode           = Input::get('zipcode');
+            $city->city                 = Input::get('city');
+            $user_data->phone_number    = Input::get('phone_number');
             //$user_data->password = Hash::make(Input::get('password'));
             $user_data->user_image_path     = $filename;
+            
+            //dd($user_data->phone_number);
 
             // save our data
+            $street->save();
+            $zipcode->save();
+            $city->save();
             $user_data->save();
 
             // redirect ----------------------------------------
--- app/views/personal_settings.blade.php
+++ app/views/personal_settings.blade.php
@@ -17,10 +17,10 @@
 {{Form::text('first_name', $personal_data['first_name'], ['placeholder' => 'voornaam']) }}<br />
 {{Form::text('last_name', $personal_data['last_name'], ['placeholder' => 'achternaam']) }}<br />
 
-{{Form::text('street_name','',['placeholder' => 'Straatnaam']) }}<br />
-{{Form::text('house_number', '', ['placeholder' => 'Huisnummer']) }}<br />
-{{Form::text('zipcode','',['placeholder' => 'Postcode']) }}<br />
-{{Form::text('city','',['placeholder' => 'Woonplaats']) }}<br />
+{{Form::text('street', $personal_data['street'], ['placeholder' => 'Straatnaam']) }}<br />
+{{Form::text('house_number', $personal_data['house_number'], ['placeholder' => 'Huisnummer']) }}<br />
+{{Form::text('zipcode', $personal_data['zipcode'], ['placeholder' => 'Postcode']) }}<br />
+{{Form::text('city', $personal_data['city'] ,['placeholder' => 'Woonplaats']) }}<br />
 {{Form::text('phone_number', $personal_data['phone_number'], ['placeholder' => 'telefoonnummer']) }}<br />
 
 {{Form::submit('Opslaan')}}
