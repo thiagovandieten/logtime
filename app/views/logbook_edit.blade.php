a49b42c2730f8fef9fbb4427e2ee5f4f2034bb13
SirWhale <philiph3@hotmail.com>
2015-01-19 10:32:37 +0100

    moedls en migration gefixed na merge

--- app/views/logbook_edit.blade.php
+++ app/views/logbook_edit.blade.php
@@ -1,22 +1,16 @@
 @extends('template.main')
 @section('content')
 <div class='topbatr'>
-
-
-
-
-{{var_dump($userlog)}};
-
-{{Form::open(array('route' => 'logboek.store', 'method' => 'POST')) }}
-              {{Form::select('log categorie' , $userProjects['Log_Categories'])}}
-              {{Form::select('project' , $userProjects['Projects'])}}
-              {{Form::select('categorie' , $userProjects['Categories'])}}
-              {{Form::select('taak' , $userProjects['Tasks'])}}
-              {{Form::text('date' , date('Y/m/d') )}}
-              {{Form::text('starttijd' , '00:00' , null , ['class' => 'uren'])}}
-              {{Form::text('stoptijd' , '00:00' , null , ['class' => 'uren'])}}
-              {{Form::textarea('omschrijving' , 'Omschrijving')}}
-            {{Form::submit('Bijwerken',['class' => 'Bijwerken'])}}
-          {{Form::close() }}
+  {{Form::open(array('route' => 'logboek.store', 'method' => 'POST')) }}
+      {{Form::select('log categorie' , $userProjects['Log_Categories'])}}
+      {{Form::select('project' , $userProjects['Projects'])}}
+      {{Form::select('categorie' , $userProjects['Categories'])}}
+      {{Form::select('taak' , $userProjects['Tasks'])}}
+      {{Form::text('date' , date('Y/m/d') )}}
+      {{Form::text('starttijd' , '00:00' , null , ['class' => 'uren'])}}
+      {{Form::text('stoptijd' , '00:00' , null , ['class' => 'uren'])}}
+      {{Form::textarea('omschrijving' , 'Omschrijving')}}
+    {{Form::submit('Bijwerken',['class' => 'Bijwerken'])}}
+  {{Form::close() }}
 </div>
 @stop
